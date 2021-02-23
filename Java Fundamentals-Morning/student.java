671ab54c933b3bd29928697eb27b0f8e3e9b6c24
govind-singhal <79433309+govind-singhal@users.noreply.github.com>
2021-02-23 21:02:42 +0530

    Create student.java

---
+++ Java Fundamentals-Morning/student.java
@@ -0,0 +1,25 @@
+public class student {
+	private int roll;
+	private int marks;
+	private String subject;
+	public student() {}
+	public int getRollno() {
+		return roll;
+	}
+	public void setRollno(int roll) {
+		this.roll = roll;
+	}
+	public int getMarks() {
+		return marks;
+	}
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+}
